5405805d12b59acc0187681d8ed5f949345c18bd
ChoiBoiii <Isaakchoi@gmail.com>
2023-09-03 22:47:12 +1000

    .

---
+++ tests/strip-comments.c
@@ -0,0 +1,38 @@
+// #include <stdio.h>
+
+#define TEXT "RAN" // SOME TEST /*
+
+unsigned char some_func(int a, char b) { // SOME FUNCTION // // // /// / 
+    return a + b;
+
+}
+
+int main() {
+    // printf(TEXT "\n \
+    // ergthdy d");
+  
+    //SOme */
+    /**
+    test ewgfoager hq 984yh \ \ wfr \addindex
+     \
+     wefrgsthrd esrg 
+    //  / *
+
+     // COMMENT HERE
+
+     / * */1 + 2; /* fgghjty*/
+    //*/
+
+    // /*
+
+    // 3r4ty56u
+
+    // */
+
+    int a = 'cccccc';
+
+    printf("%d\n", a);
+
+    return \
+    0;
+}
