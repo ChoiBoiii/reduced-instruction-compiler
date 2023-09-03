27bcd933655346bcf8eece960cc2a7473fd03470
ChoiBoiii <Isaakchoi@gmail.com>
2023-09-03 22:49:25 +1000

    .

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-// #include <stdio.h>
+#include <stdio.h>
 
 #define TEXT "RAN" // SOME TEST /*
 
@@ -8,28 +8,9 @@ unsigned char some_func(int a, char b) { // SOME FUNCTION // // // /// /
 }
 
 int main() {
-    // printf(TEXT "\n \
-    // ergthdy d");
-  
-    //SOme */
-    /**
-    test ewgfoager hq 984yh \ \ wfr \addindex
-     \
-     wefrgsthrd esrg 
-    //  / *
 
-     // COMMENT HERE
-
-     / * */1 + 2; /* fgghjty*/
-    //*/
-
-    // /*
-
-    // 3r4ty56u
-
-    // */
-
-    int a = 'cccccc';
+    int a = 'ccc
+    c';
 
     printf("%d\n", a);
 
