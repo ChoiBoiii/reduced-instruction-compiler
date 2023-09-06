e30348c12048eb2f84b7d6103b44b98c67bec296
ChoiBoiii <Isaakchoi@gmail.com>
2023-09-06 18:42:48 +1000

    .

--- emulator/main.c
+++ emulator/main.c
@@ -23,17 +23,30 @@ void print_register(void* ptr) {
 // Decomposed instructions
 #define NAND(X, Y) ( ~(X & Y) )
 
-#define AND(X, Y)  ( NAND(NAND(X, Y), NAND(X, Y)))
-#define OR(X, Y)   ( NAND(NAND(X, X), NAND(Y, Y)))
-#define NOT(X)     ( NAND(X, X))
+#define AND(X, Y)  ( NAND(NAND(X, Y), NAND(X, Y)) )
+#define OR(X, Y)   ( NAND(NAND(X, X), NAND(Y, Y)) )
+#define NOT(X)     ( NAND(X, X)                   )
+#define XOR(X, Y)  ( AND(OR(X, Y), NAND(X, Y))    )
+
+reg_t ADD(X, Y) {
+    return ((AND(X, Y) << 1) != 0) ? ADD((AND(X, Y) << 1), XOR(X, Y)) : XOR(X, Y);
+}
+
+// reg_t ADD(X, Y) {
+//     reg_t keep = (X & Y) << 1;
+//     reg_t res = X ^ Y;
+//     return (keep) ? ADD(keep, res) : res;
+// }
 
 
 // Execute
 int main() {
 
-    reg_t x = 0b1010;
-    reg_t y = 0b1001;
-
+    reg_t x = 121;
+    reg_t y = 121;
+    reg_t result = ADD(x, y);
+    printf("%d\n", result);
+    // print_register(&result);
 
     // Return
     return 0;
