da9375760a54a265133e8bf731f571f04121c95f
ChoiBoiii <Isaakchoi@gmail.com>
2023-09-08 01:17:01 +1000

    .

--- reduced-instruction-library/test.c
+++ reduced-instruction-library/test.c
@@ -1,30 +1,21 @@
-#include <stdio.h>
-#include <boost/preprocessor/repetition/repeat.hpp>
+
+#define BOOST_PP_LIMIT_REPEAT (1024) // Configure boost header to allow up to 1024 repeats. Max allowed by header is 1024.
+#include <boost/preprocessor/arithmetic/div.hpp>
+#include "boost/preprocessor/repetition/repeat.hpp"
+#include "boost/preprocessor/arithmetic/add.hpp"
+#include "boost/preprocessor/arithmetic/sub.hpp"
+#include "boost/preprocessor/arithmetic/mul.hpp"
+#include "boost/preprocessor/arithmetic/mod.hpp"
+#include "boost/preprocessor/arithmetic/div.hpp"
+
+#define Fold(z, n, text)  text                       // Helper function for STRREP
+#define STRREP(str, n) BOOST_PP_REPEAT(n, Fold, str) // Call 'STRREP' to repeat 'str' 'n' times
+
+BOOST_PP_DIV(14, 5) // expands to 2
+BOOST_PP_DIV(15, 5) // expands to 2
+BOOST_PP_DIV(16, 5) // expands to 2
 
 
-// Base operators (not NAND based)
-// #define NAND(X, Y)    ( ~(X & Y)                     ) 
-// #define BSL(X, N)     ( (X << N)                     ) 
-// #define BSR(X, N)     ( (X >> N)                     ) 
-
-// // Other decomposed bitwise operators
-// #define AND(X, Y)     ( NAND(NAND(X, Y), NAND(X, Y)) ) 
-// #define OR(X, Y)      ( NAND(NAND(X, X), NAND(Y, Y)) ) 
-// #define NOT(X)        ( NAND(X, X)                   ) 
-// #define XOR(X, Y)     ( AND(OR(X, Y), NAND(X, Y))    ) 
-
-#define UINT_ADD_HELPER(Z, N) ({       \
-    tmp = keep;                        \
-    keep = BSL(AND(keep, res), 1);     \
-    res = XOR(tmp, res);               \
-})
 
 
-int main(){
 
-    u_int16_t a = 0xFFFF;
-    u_int16_t b = 0x0001;
-    printf("%hu\n%hu\n", a+b, (u_int16_t)(a+b));
-
-    return 0;
-}
