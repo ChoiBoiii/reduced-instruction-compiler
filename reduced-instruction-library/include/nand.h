305317d3cc4a6b9c264e7044df2959893d5852cc
ChoiBoiii <Isaakchoi@gmail.com>
2023-09-09 23:26:49 +1000

    .

--- reduced-instruction-library/include/nand.h
+++ reduced-instruction-library/include/nand.h
@@ -203,7 +203,7 @@ typedef RIC_TMP_CONFIG_REGISTER_TYPE reg_t;             // The type to use to st
 
 // HELPER: Returns a formatted fold line for the EXTRACT_MSB_EQ_HELPER method
 #define HELPER_1(X, S)              \
-    (ifmask |= (ifmask << S));
+    (X |= (X << S));
 
 // HELPER: Returns the ammount of bitshift required for an iteration of the EXTRACT_MSB_EQ_HELPER method
 #define HELPER_2(N)              \
