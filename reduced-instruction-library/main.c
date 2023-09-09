8930c5ebf9692b1838d2a0daad42599a085e6edd
ChoiBoiii <Isaakchoi@gmail.com>
2023-09-10 01:38:01 +1000

    .

--- reduced-instruction-library/main.c
+++ reduced-instruction-library/main.c
@@ -17,13 +17,6 @@ int main() {
     // RUN TESTS
     run_all_tests();
 
-    //
-    reg_t a = 19222;
-    reg_t b = 65223;
-    reg_t c = UINT_MULT(a, b);
-    printf("%hu %u\n", c, ((int)a * (int)b) % 65536);
-    print_register(&c);
-
     // EXIT
     return 0;
 }
--- reduced-instruction-library/test.c
+++ reduced-instruction-library/test.c
@@ -11,6 +11,13 @@ int main() {
     // RUN TESTS
     run_all_tests();
 
+    // CUSTOM TEST
+    signed_reg_type a = 3523;
+    signed_reg_type b = -13;
+    signed_reg_type c = UINT_MULT(a, b);
+    printf("%hd %hd\n", c, (reg_t)(((int)a * (int)b) % 65536));
+    print_register(&c);
+
     // EXIT
     return 0;
     
