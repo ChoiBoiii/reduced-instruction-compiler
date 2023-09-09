776a0ad844bec2cb8d3ba1c66df0358fc48fe7d6
ChoiBoiii <Isaakchoi@gmail.com>
2023-09-10 02:13:19 +1000

    .

--- reduced-instruction-library/include/nand.h
+++ reduced-instruction-library/include/nand.h
@@ -328,7 +328,7 @@ typedef RIC_TMP_CONFIG_REGISTER_TYPE reg_t;             // The type to use to st
 
 // Returns 1 if X > Y {OPTIMISE}
 #define UINT_GTHAN(X, Y) ({                             \
-    reg_t _ric_out = X ^ Y;                             \
+    reg_t _ric_out = BW_XOR(X, Y);                      \
     _ric_out = GENERATE_MASK_UP_TO_MSB(_ric_out);       \
     _ric_out = UINT_SUB(_ric_out, BW_BSR(_ric_out, 1)); \
     _ric_out = BW_XOR(BW_AND(Y, _ric_out), _ric_out);   \
