f4b08d4ec837bbca85d8e7e338b9f08488f49acf
ChoiBoiii <Isaakchoi@gmail.com>
2023-09-10 01:32:27 +1000

    .

---
+++ reduced-instruction-library/test.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdbool.h>
+
+// Import tests
+#include "tests.h"
+
+// Execute
+int main() {
+
+    // RUN TESTS
+    run_all_tests();
+
+    // EXIT
+    return 0;
+    
+}
+
+
